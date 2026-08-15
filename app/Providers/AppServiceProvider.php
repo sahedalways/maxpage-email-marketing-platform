c14d0b17d23345dc94b161c54ca7b2a5a83ac7d0
sahedalways <ssahed65@gmail.com>
2026-08-16 00:58:27 +0600

    Load Debugbar provider conditionally so production installs work

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment('local') && class_exists(\Barryvdh\Debugbar\ServiceProvider::class)) {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        }
     }
 
     /**
--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
         Milon\Barcode\BarcodeServiceProvider::class,
 
 
